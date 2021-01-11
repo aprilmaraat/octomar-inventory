2f9533ed2abbfafb7f80187e2f0c23b4951cdee7
April John Maraat <aprilmaraat@gmail.com>
2021-01-11 23:20:29 +0800

    Modified api.php

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,36 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::resource('customer', 'CustomerController');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('supplier', 'SupplierController');
+
+Route::get('item/with_total_inventory', 'ItemController@withTotalInventory');
+
+Route::resource('item', 'ItemController');
+
+Route::post('item_inventory/store_batch', 'ItemInventoryController@storeBatch');
+
+Route::delete('item_inventory/transaction', 'ItemInventoryController@deleteByTransaction');
+
+Route::resource('item_inventory', 'ItemInventoryController');
+
+Route::resource('sales_invoice', 'SalesInvoiceController');
+
+Route::post('sales_invoice_detail/store_batch', 'SalesInvoiceDetailController@storeBatch');
+
+Route::delete('sales_invoice_detail/transaction', 'SalesInvoiceDetailController@deleteByTransaction');
+
+Route::resource('sales_invoice_detail', 'SalesInvoiceDetailController');
+
+Route::resource('item_inventory_adjustment', 'ItemInventoryAdjustmentController');
+
+Route::post('item_inventory_adjustment_detail/store_batch', 'ItemInventoryAdjustmentDetailController@storeBatch');
+
+Route::delete('item_inventory_adjustment_detail/transaction', 'ItemInventoryAdjustmentDetailController@deleteByTransaction');
+
+Route::resource('item_inventory_adjustment_detail', 'ItemInventoryAdjustmentDetailController');
+
+Route::post('option/auto_increment', 'OptionController@setAutoIncrement');
+Route::get('option', 'OptionController@getOptions');
+Route::post('option', 'OptionController@setOption');
